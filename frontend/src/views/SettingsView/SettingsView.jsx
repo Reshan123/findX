e7b8e7fc1ae717a45cc7aec5f13ee9640584c5c7
Reshan Gomis <reshangomis@gmail.com>
2024-06-30 17:40:27 +0530

    Create Settings View

--- frontend/src/views/SettingsView/SettingsView.jsx
+++ frontend/src/views/SettingsView/SettingsView.jsx
@@ -3,7 +3,10 @@ import './SettingsView.css'
 const SettingsView = () => {
     return ( 
         <div className="settingsView">
-            
+            <div className="topContainer">
+                <div className="title">Settings</div>
+                <div className="subText">Update your profile information</div>
+            </div>
         </div>
      );
 }
